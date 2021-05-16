ad0d61301cc97dcbd46bdcf7d09cd04d6de16ccb
baegteun <baegteun@gmail.com>
2021-05-17 08:05:03 +0900

    [백준] 1427 소트인사이드
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    2 초	128 MB	35498	21484	18163	61.138%
    문제
    배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
    
    입력
    첫째 줄에 정렬하고자하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
    
    출력
    첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
    
    예제 입력 1
    2143
    예제 출력 1
    4321

---
+++ src/B_1427/B_1427.kt
@@ -0,0 +1,20 @@
+package B_1427
+
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var N = br.readLine().toInt()
+    val arr = ArrayList<Int>()
+    while (N > 0) {
+        arr.add(N % 10)
+        N /= 10
+    }
+    arr.sort()
+    for (i in arr.indices.reversed()) {
+        bw.write(arr[i].toString())
+        bw.flush()
+    }
+}
