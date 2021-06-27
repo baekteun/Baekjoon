cdb54dda458e40a9f7974125f622c1f1e7513ddb
baegteun <baegteun@gmail.com>
2021-06-28 07:41:45 +0900

    [백준] 3197 백조의 호수
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초	256 MB	12397	2591	1485	19.174%
    문제
    두 마리의 백조가 호수에서 살고 있었다. 그렇지만 두 마리는 호수를 덮고 있는 빙판으로 만나지 못한다.
    
    호수는 행이 R개, 열이 C개인 직사각형 모양이다. 어떤 칸은 얼음으로 덮여있다.
    
    호수는 차례로 녹는데, 매일 물 공간과 접촉한 모든 빙판 공간은 녹는다. 두 개의 공간이 접촉하려면 가로나 세로로 닿아 있는 것만 (대각선은 고려하지 않는다) 생각한다.
    
    아래에는 세 가지 예가 있다.
    
    ...XXXXXX..XX.XXX ....XXXX.......XX .....XX..........
    ....XXXXXXXXX.XXX .....XXXX..X..... ......X..........
    ...XXXXXXXXXXXX.. ....XXX..XXXX.... .....X.....X.....
    ..XXXXX..XXXXXX.. ...XXX....XXXX... ....X......XX....
    .XXXXXX..XXXXXX.. ..XXXX....XXXX... ...XX......XX....
    XXXXXXX...XXXX... ..XXXX.....XX.... ....X............
    ..XXXXX...XXX.... ....XX.....X..... .................
    ....XXXXX.XXX.... .....XX....X..... .................
          처음               첫째 날             둘째 날
    백조는 오직 물 공간에서 세로나 가로로만(대각선은 제외한다) 움직일 수 있다.
    
    며칠이 지나야 백조들이 만날 수 있는 지 계산하는 프로그램을 작성하시오.
    
    입력
    입력의 첫째 줄에는 R과 C가 주어진다. 단, 1 ≤ R, C ≤ 1500.
    
    다음 R개의 줄에는 각각 길이 C의 문자열이 하나씩 주어진다. '.'은 물 공간, 'X'는 빙판 공간, 'L'은 백조가 있는 공간으로 나타낸다.
    
    출력
    첫째 줄에 문제에서 주어진 걸리는 날을 출력한다.
    
    예제 입력 1
    10 2
    .L
    ..
    XX
    XX
    XX
    XX
    XX
    XX
    ..
    .L
    예제 출력 1
    3
    예제 입력 2
    4 11
    ..XXX...X..
    .X.XXX...L.
    ....XXX..X.
    X.L..XXX...
    예제 출력 2
    2
    예제 입력 3
    8 17
    ...XXXXXX..XX.XXX
    ....XXXXXXXXX.XXX
    ...XXXXXXXXXXXX..
    ..XXXXX.LXXXXXX..
    .XXXXXX..XXXXXX..
    XXXXXXX...XXXX...
    ..XXXXX...XXX....
    ....XXXXX.XXXL...
    예제 출력 3
    2

---
+++ src/B_3197/B_3197.java
@@ -0,0 +1,139 @@
+package B_3197;
+import java.util.*;
+import java.io.*;
+public class B_3197 {
+
+    private static int R;
+    private static int C;
+    private static char[][] map;
+    private static int[][] melt;
+    private static int day;
+    private static int[] x_move = {1, 0, -1, 0};
+    private static int[] y_move = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String firstLine = in.readLine();
+
+        R = Integer.parseInt(firstLine.split(" ")[0]);
+        C = Integer.parseInt(firstLine.split(" ")[1]);
+
+        map = new char[R][C];
+        List<Point> swan = new ArrayList<>();
+
+        for (int i = 0; i < R; i++) {
+            String input = in.readLine();
+
+            for (int j = 0; j < C; j++) {
+                map[i][j] = input.charAt(j);
+
+                if (map[i][j] == 'L') {
+                    swan.add(new Point(i, j));
+                }
+            }
+        }
+
+        in.close();
+
+        melt = melt();
+
+        int left = 0;
+        int right = day;
+        int min = Integer.MAX_VALUE;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isReachable(swan.get(0), swan.get(1), mid)) {
+                min = Math.min(min, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    private static boolean isReachable(Point point1, Point point2, int limit) {
+        boolean[][] visited = new boolean[R][C];
+
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(point1);
+        visited[point1.x][point2.y] = true;
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int new_x = point.x + x_move[k];
+                int new_y = point.y + y_move[k];
+                if (inArea(new_x, new_y) && !visited[new_x][new_y] && melt[new_x][new_y] <= limit) {
+                    visited[new_x][new_y] = true;
+
+                    if (new_x == point2.x && new_y == point2.y) {
+                        return true;
+                    }
+
+                    queue.offer(new Point(new_x, new_y));
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static int[][] melt() {
+        boolean[][] visited = new boolean[R][C];
+        int[][] melt = new int[R][C];
+
+        Queue<Point> queue = new LinkedList<>();
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 'L' || map[i][j] == '.') {
+                    queue.offer(new Point(i, j));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        day = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            day++;
+
+            for (int i = 0; i < size; i++) {
+                Point point = queue.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    int new_x = point.x + x_move[j];
+                    int new_y = point.y + y_move[j];
+                    if (inArea(new_x, new_y) && !visited[new_x][new_y] && map[new_x][new_y] == 'X') {
+                        melt[new_x][new_y] = day;
+                        visited[new_x][new_y] = true;
+
+                        queue.offer(new Point(new_x, new_y));
+                    }
+                }
+            }
+        }
+
+        return melt;
+    }
+
+    private static boolean inArea(int x, int y) {
+        return x >= 0 && y >= 0 && x < R && y < C;
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
