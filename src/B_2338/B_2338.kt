b44de84832c797dc8f705d4ee8a9ff93bc3cfc87
baekteun <baegteun@gmail.com>
2021-09-12 17:32:44 +0900

    [백준] 2338 긴자리 계산
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    2 초	128 MB	8824	3741	3438	51.375%
    문제
    두 수 A, B를 입력받아, A+B, A-B, A×B를 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 A가, 둘째 줄에 B가 주어진다. 각각의 수는 10진수로 1,000자리를 넘지 않으며 양수와 음수가 모두 주어질 수 있다.
    
    출력
    첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A×B를 출력한다. 각각을 출력할 때, 답이 0인 경우를 제외하고는 0으로 시작하게 해서는 안 된다(1을 01로 출력하면 안 된다는 의미).
    
    예제 입력 1
    1
    -1
    예제 출력 1
    0
    2
    -1

---
+++ src/B_2338/B_2338.kt
@@ -0,0 +1,12 @@
+package B_2338
+
+import java.util.*
+
+fun main(args: Array<String>){
+    val sc = Scanner(System.`in`)
+    val s1 = sc.nextBigInteger()
+    val s2 = sc.nextBigInteger()
+    println(s1.add(s2))
+    println(s1.subtract(s2))
+    println(s1.multiply(s2))
+}
