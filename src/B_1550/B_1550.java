753c848719e12491fed78c1e531a486198e534f1
baekteun <baegteun@gmail.com>
2021-09-12 17:11:33 +0900

    [백준] 1550 16진수 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율 2 초	128 MB	13561	8737	7760	65.402% 문제 16진수 수를 입력받아서 10진수로 출력하는 프로그램을 작성하시오.
    입력
    첫째 줄에 16진수 수가 주어진다. 이 수의 최대 길이는 6글자이다. 16진수 수는 0~9와 A~F로 이루어져 있고, A~F는 10~15를 뜻한다. 또, 이 수는 음이 아닌 정수이다.
    
    출력
    첫째 줄에 입력으로 주어진 16진수 수를 10진수로 변환해 출력한다.
    
    예제 입력 1
    A
    예제 출력 1
    10

---
+++ src/B_1550/B_1550.java
@@ -0,0 +1,11 @@
+package B_1550;
+
+import java.io.*;
+
+public class B_1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(Integer.parseInt(s,16));
+    }
+}
