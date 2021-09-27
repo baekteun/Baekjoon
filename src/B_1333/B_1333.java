b56004781157202b3f9939302e34324697a093d8
baekteun <baegteun@gmail.com>
2021-09-27 19:01:55 +0900

    [백준] 1333 부재중 전화 - Java
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    2 초	128 MB	3208	740	629	26.252%
    문제
    얼마전, Day Of Mourning의 새 앨범이 나왔고, 강토는 이 앨범을 들으려고 한다.
    
    이 앨범에는 총 노래가 N곡이 들어있고, 모든 노래의 길이는 L초이다. 그리고, 노래와 노래 사이에는 5초 동안 아무 노래도 들리지 않는 조용한 구간이 있다.
    
    강토가 앨범의 첫 곡을 듣는 순간이 0초이다. 그리고 그 0초부터 강토의 전화벨이 울리기 시작한다. 전화벨은 D초에 1번씩 울리며, 한 번 울릴 때 1초동안 울린다.
    
    강토는 락 스피릿을 진심으로 느끼기 위해서 볼륨을 매우 크게 하고 듣기 때문에, 노래가 나오는 중에는 전화벨 소리를 듣지 못한다.
    
    만약, 전화벨이 노래가 시작되는 순간 울린다면, 강토는 전화를 받지 못한다. 또, 전화벨이 노래가 끝나는 순간 같이 끝난다면, 강토는 전화를 받을 수 없다.
    
    강토는 앨범을 1번만 듣는다. 즉, 모든 앨범 수록곡을 다 듣고 난 후에는 전화벨을 들을 수 있다.
    
    강토가 전화벨을 들을 수 있는 가장 빠른 시간을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 N, L, D가 공백을 사이에 두고 주어진다. 모든 수는 1,000보다 작거나 같은 자연수이다.
    
    출력
    첫째 줄에 강토가 전화벨을 들을 수 있는 가장 빠른 시간을 출력한다.
    
    예제 입력 1
    2 5 7
    예제 출력 1
    7

--- src/B_1333/B_1333.java
+++ src/B_1333/B_1333.java
@@ -14,18 +14,18 @@ public class B_1333 {
         ArrayList<Boolean> enable = new ArrayList<>();
 
         for (int i = 0; i < N; ++i) {
-            for(int j = 0; j < L; j++) {    //음악 n초 출력
+            for(int j = 0; j < L; j++) {
                 enable.add(true);
             }
 
-            if(i != N-1) { // 마지막 곡이 아닐 경우
-                for(int j = 0; j < 5; j++) {    // 음악과 음악 사이 5초
+            if(i != N-1) {
+                for(int j = 0; j < 5; j++) {
                     enable.add(false);
                 }
             }
         }
         int res = 0;
-        while(res < enable.size()) {    //벨이 울리는 주기에 따라 해당 sec를 확인하여 음악이 나오지 않는중(false)이면 해당 sec를 출력
+        while(res < enable.size()) {
             if(!enable.get(res)) {
                 break;
             }
