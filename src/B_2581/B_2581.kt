e3d5e2f3e7525c739fb509834d3c326ad6e15873
baekteun <baegteun@gmail.com>
2021-10-04 16:53:46 +0900

    [백준] 2581 소수 - Kotlin
    시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
    1 초	128 MB	61741	23688	20366	39.044%
    문제
    자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
    
    예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
    
    입력
    입력의 첫째 줄에 M이, 둘째 줄에 N이 주어진다.
    
    M과 N은 10,000이하의 자연수이며, M은 N보다 작거나 같다.
    
    출력
    M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다.
    
    단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
    
    예제 입력 1
    60
    100
    예제 출력 1
    620
    61
    예제 입력 2
    64
    65
    예제 출력 2
    -1

---
+++ src/B_2581/B_2581.kt
@@ -0,0 +1,39 @@
+package B_2581
+
+import java.io.*
+import kotlin.system.exitProcess
+
+val eratos = BooleanArray(10_001){false}
+fun main()=with(BufferedReader(InputStreamReader(System.`in`))){
+    setEra()
+    var x: Int
+    var y: Int
+    readLine().let { x = it.toInt() }
+    readLine().let { y = it.toInt() }
+    var res = 0
+    var min = 0
+
+
+    for (i in y downTo x){
+        if(!eratos[i]) {
+            res+=i
+            min = i
+        }
+    }
+    if(res==0){
+        print(-1)
+        exitProcess(0)
+    }
+    println(res)
+    print(min)
+}
+
+fun setEra(){
+    eratos[0] = true
+    eratos[1] = true
+    for (i in 1 until eratos.size){
+        if(!eratos[i])
+            for(k in i*i until eratos.size step i)
+                eratos[k] = true
+    }
+}
